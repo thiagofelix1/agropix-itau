154893e1167cbdf75cc57ce47f4d5d09c55959bc
thiagofelix1 <thiagofelixdasilva099@gmail.com>
2022-12-09 16:00:30 -0400

    first commit

---
+++ src/test/java/com/agropix/itau/ItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agropix.itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
